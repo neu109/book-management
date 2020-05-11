229417521039414957b488a347c4322a07b1beeb
Yuho Osonoi <osonoi@chinoiyuufunoMacBook-Air.local>
2020-05-11 21:23:42 +0900

    add test draft

--- src/main/kotlin/example/BookController.kt
+++ src/main/kotlin/example/BookController.kt
@@ -1,45 +1,45 @@
 package example
 
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Post
-import io.micronaut.http.annotation.QueryValue
+import io.micronaut.http.annotation.*
 import javax.inject.Inject
 
 @Controller("/")
 class BookController (@Inject val bookRepository: BookRepository,@Inject val searchRepository: SearchRepository){
 
     @Post("/add")
-    fun addNewBook(title: String,author: String,attribute: String):String{
+    fun addNewBook(title: String, author: String, attribute: String):String{
         bookRepository.save(Book(0,title,author,attribute))
         return "Saved"
     }
 
-    @Get("/find_all")
+    @Get("/find")
     fun findAllList()=bookRepository.findAll()
 
-    @Get("/find_id")
-    fun findListById(@QueryValue id: Long)=bookRepository.findById(id)
+    @Get("/find/{id}")
+    fun findListById(id: Long)=bookRepository.findById(id)
 
     @Post("/update")
-    fun updateBook(@QueryValue id: Long,title:String,author:String,attribute:String):String{
-        bookRepository.save(Book(id, title, author,attribute))
-        return "Updated"
+    fun updateBook(id: Long, title:String, author:String, attribute:String):String{
+        if (bookRepository.existsById(id)==true) {
+            bookRepository.update(Book(id, title, author, attribute))
+            return "Updated"
+        }else
+            return "No Data"
     }
 
     @Post("/delete")
-    fun deleteBook(@QueryValue id: Long):String{
+    fun deleteBook(id: Long):String{
         if (bookRepository.existsById(id)==true) {
             bookRepository.deleteById(id)
             return "Deleted"
         }else
-            return "No Book"
+            return "No Data"
     }
 
     @Get("/find_author")
-    fun findListByAuthor(@QueryValue author: String)=searchRepository.findByAuthor(author)
+    fun findListByAuthor(author: String)=searchRepository.findByAuthor(author)
 
     @Get("/find_title")
-    fun findListByTitle(@QueryValue title: String)=searchRepository.findByTitle(title)
+    fun findListByTitle(title: String)=searchRepository.findByTitle(title)
 
 }
